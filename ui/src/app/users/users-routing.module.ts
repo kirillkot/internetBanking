eb1b888efa21745a6c3fe1cc5922813c50a47ea1
Anton Grabko <anton.grabko@softswiss.com>
2017-12-01 20:36:00 +0300

    ui: users: configure list component

--- ui/src/app/users/users-routing.module.ts
+++ ui/src/app/users/users-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { UserFormComponent } from './user-form.component';
+import { UserListComponent } from './user-list.component';
 
 
 const routes: Routes = [
-    { path: 'users/form', component: UserFormComponent },
+  { path: 'users', component: UserListComponent },
+  { path: 'users/form', component: UserFormComponent },
 ];
 
 @NgModule({
--- ui/src/app/users/users.module.ts
+++ ui/src/app/users/users.module.ts
@@ -7,6 +7,7 @@ import { MaterialModule } from '../material/material.module';
 import { UsersRoutingModule } from './users-routing.module';
 import { UserService } from './user.service';
 import { UserFormComponent } from './user-form.component';
+import { UserListComponent } from './user-list.component';
 
 @NgModule({
   imports: [
@@ -15,7 +16,7 @@ import { UserFormComponent } from './user-form.component';
     MaterialModule,
     UsersRoutingModule
   ],
-  declarations: [UserFormComponent],
+  declarations: [UserFormComponent, UserListComponent],
   providers: [UserService]
 })
 export class UsersModule { }
