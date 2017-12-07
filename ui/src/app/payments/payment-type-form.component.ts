3d661b678a12c7cc8fc85b18af559ff4e2d5182c
Anton Grabko <softforlutshix@gmail.com>
2017-12-07 15:07:34 +0300

    ui: payment: type: add simple form

--- ui/src/app/payments/payment-type-form.component.ts
+++ ui/src/app/payments/payment-type-form.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+import { FormComponent } from '../abstract/form.component';
+
+import { PaymentTypeForm, PaymentType } from './payment-type.service';
+import { PaymentTypeService } from './payment-type.service';
+
 
 @Component({
   selector: 'app-payment-type-form',
   templateUrl: './payment-type-form.component.html',
   styleUrls: ['./payment-type-form.component.css']
 })
-export class PaymentTypeFormComponent implements OnInit {
+export class PaymentTypeFormComponent extends
+    FormComponent<PaymentTypeForm, PaymentType> {
+  constructor(
+    location: Location,
+    formbuilder: FormBuilder,
+    service: PaymentTypeService,
+  ) {
+    super(location, formbuilder, service);
+  }
 
-  constructor() { }
-
-  ngOnInit() {
+  fields(): any {
+    return {
+      name: ['', [Validators.required, Validators.minLength(4)]],
+      type: ['', [Validators.required, Validators.minLength(2)]],
+      commision: [0, [Validators.required]],
+      account_id: [0, []],
+      detail: ['', []],
+    };
   }
 
 }
