b6213501423878bfc64ee83756a769e3bbad191c
Anton Grabko <softforlutshix@gmail.com>
2017-12-18 20:40:20 +0300

    ui: currencies: add simple form

--- ui/src/app/currencies/currency-form.component.ts
+++ ui/src/app/currencies/currency-form.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+import { FormComponent } from '../abstract/form.component';
+
+import { CurrencyForm, Currency, CurrencyService } from './currency.service';
+
 
 @Component({
   selector: 'app-currency-form',
   templateUrl: './currency-form.component.html',
   styleUrls: ['./currency-form.component.css']
 })
-export class CurrencyFormComponent implements OnInit {
+export class CurrencyFormComponent extends
+    FormComponent<CurrencyForm, Currency> {
 
-  constructor() { }
+  constructor(
+    location: Location,
+    formbuilder: FormBuilder,
+    service: CurrencyService,
+  ) {
+    super(location, formbuilder, service);
+  }
 
-  ngOnInit() {
+  fields(): any {
+    return {
+      name: ['', [Validators.required]],
+      sale: [null, [Validators.required]],
+      purchase: [null, [Validators.required]],
+    }
   }
 
 }
